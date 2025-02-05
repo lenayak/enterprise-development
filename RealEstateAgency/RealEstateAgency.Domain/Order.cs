6fabd43934b7371fe950fdf7dabe3bd843f4cb67
lenayak <113600536+lenayak@users.noreply.github.com>
2025-02-06 02:03:30 +0400

    classes have been rewritten

---
+++ RealEstateAgency/RealEstateAgency.Domain/Order.cs
@@ -0,0 +1,43 @@
+namespace RealEstateAgency.Domain;
+
+public enum TypeOrder
+{
+    Purchase,
+    Sale
+}
+
+/// <summary>
+/// Заявка клиента
+/// </summary>
+public class Order
+{
+    /// <summary>
+    /// Идентификатор заявки
+    /// </summary>
+    public required int Id { get; set; }
+
+    /// <summary>
+    /// Клиент, сделавший заявку
+    /// </summary>
+    public required Client Client { get; set; }
+
+    /// <summary>
+    /// Время заявки
+    /// </summary>
+    public required DateTime Time { get; set; }
+
+    /// <summary>
+    /// Тип заявки (покупка или продажа)
+    /// </summary>
+    public required TransactionType Type { get; set; }
+
+    /// <summary>
+    /// Объект недвижимости
+    /// </summary>
+    public required RealEstate Item { get; set; }
+
+    /// <summary>
+    /// Цена недвижимости
+    /// </summary>
+    public required decimal Price { get; set; }
+}
